2d2e6d6d5e73311e80f14a48ed2837301b7fd53d
dalcengio.gabriele@gmail.com <gdalceng@sfu.ca>
2021-10-19 17:35:48 -0700

    Implement filter function on search-bar change

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Search from "./Search";
 
 function App() {
   const [studentData, setStudentData] = useState(null);
+  const [students, setStudents] = useState([]);
 
   async function fetchStudentData() {
     try {
@@ -18,9 +19,37 @@ function App() {
     }
   }
 
+  function handleSearch(event) {
+    //get data
+    const query = event.target.value.toLowerCase().trim();
+    const students = studentData.students;
+
+    //filter data
+    const regex = new RegExp(query, "i");
+    // console.log(regex);
+    const result = students.filter((student) => {
+      const fullName = `${student.firstName} ${student.lastName}`;
+      return fullName.search(regex) !== -1;
+    });
+
+    //set students array
+    setStudents(result);
+
+    // console.log(query);
+    // console.log(result);
+  }
+
+  //on load or component change
   useEffect(() => {
     fetchStudentData();
   }, []);
+
+  //on studentData change, set students
+  useEffect(() => {
+    if (studentData) {
+      setStudents(studentData.students);
+    }
+  }, [studentData]);
 
   if (!studentData) {
     return "loading...";
@@ -29,9 +58,9 @@ function App() {
   return (
     // app container helps with border-radius/overflow conflict
     <div className="app-container">
-      <Search />
+      <Search handleSearch={(event) => handleSearch(event)} />
       <div className="student-list">
-        {studentData.students.map((student, index) => (
+        {students.map((student, index) => (
           <Student key={student.id} student={student} />
         ))}
       </div>
--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,14 @@
-import { render } from "react-dom";
+// import { render } from "react-dom";
 import "./Search.css";
 
 function Search(props) {
   return (
-    <input type="text" placeholder="Search by name" className="search-bar" />
+    <input
+      type="text"
+      placeholder="Search by name"
+      className="search-bar"
+      onChange={(event) => props.handleSearch(event)}
+    />
   );
 }
 
